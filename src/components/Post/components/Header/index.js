c5cdc41c08dd56dca6ca1ba27069f619380dba6c
Prajan Acharya <prajanacharya11@gmail.com>
2021-02-17 22:40:56 +0545

    Integration of react-native-vector icons library

--- src/components/Post/components/Header/index.js
+++ src/components/Post/components/Header/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import ProfilePicture from '../../../ProfilePicture';
 import styles from './style';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Header = ({imageUri, name}) => {
     return ( 
         <View style={styles.container}>
             <ProfilePicture uri={imageUri} size={40} />
             <Text style={styles.name}>{name}</Text>
+            <Icon name="rocket" size={30} color="#900" />
         </View>
      );
 }
